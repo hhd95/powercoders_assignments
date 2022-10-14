a74656ab4ee5ec8fb5bd49757136ba48a89076a7
hhd95 <hasan.danaci@powercoders.org>
2022-10-14 10:27:03 +0200

    java exc

--- week 4/javascript Z/exc 13102022/app.js
+++ week 4/javascript Z/exc 13102022/app.js
@@ -1,28 +1,31 @@
-// function myFunction(a, b, c) {
-//   return (a + b) * 3;
+// // function myFunction(a, b, c) {
+// //   return (a + b) * 3;
+// // }
+// // console.log;
+
+
+// const person = {
+//     name: "John",
+//     age: 38,
+//     eyeColor: "green"
+//     isMarried: false
+
 // }
-// console.log;
+// document.write(person.name.length);
 
 
-const person = {
-    name: "John",
-    age: 38,
-    eyeColor: "green"
-    isMarried: false
+// // const car = {type:"Fiat", model:"500", color:"white"};
 
-}
-document.write(person.name.length);
+// let arr = [];
+// arr[0] = "Jani";
+// arr[1] = "Hege";
+// arr[2] = "Stale";
+// arr[3] = "Kai Jim";
+// arr[4] = "Borge";
+
+// console.log(arr.join()); // Jani,Hege,Stale,Kai Jim,Borge
+// arr.splice(2, 0, "Lene");
+// console.log(arr.join()); // Jani,Hege,Lene,Stale,Kai Jim,Borge
 
 
-// const car = {type:"Fiat", model:"500", color:"white"};
 
-let arr = [];
-arr[0] = "Jani";
-arr[1] = "Hege";
-arr[2] = "Stale";
-arr[3] = "Kai Jim";
-arr[4] = "Borge";
-
-console.log(arr.join()); // Jani,Hege,Stale,Kai Jim,Borge
-arr.splice(2, 0, "Lene");
-console.log(arr.join()); // Jani,Hege,Lene,Stale,Kai Jim,Borge
---
+++ week 4/learning js/script.js
@@ -0,0 +1,152 @@
+const studentList = [
+  {
+    name: "Hasan",
+    surname: "Danaci",
+    age: 41,
+    eye: "green",
+    hobby: "ice skating",
+    picture: "./pictures/hasan.jpg",
+  },
+
+  {
+    name: "Enes",
+    surname: "BORA",
+    age: 38,
+    eye: "green",
+    hobby: "photography",
+    picture: "./pictures/Enes.jpg",
+  },
+  {
+    name: "Ozzy",
+    surname: "the star",
+    age: 37,
+    eye: "green",
+    hobby: "playing guitar",
+    picture: "./pictures/Ozzy.jpg",
+  },
+  {
+    name: "Ali",
+    surname: "Göçmen",
+    age: 35,
+    eye: "hazel",
+    hobby: "paint",
+    picture: "./pictures/aligocmen.jpg",
+  },
+  {
+    name: "Bilal",
+    surname: "Suleymani",
+    age: 20,
+    eye: "black",
+    hobby: "swimming",
+    picture: "./pictures/bilal.jpg",
+  },
+  {
+    name: "Jyothi ",
+    surname: "Jogin",
+    age: 23,
+    eye: "green",
+    hobby: "ice skating",
+    picture: "./pictures/jyothi.jpg",
+  },
+
+  {
+    name: "İsmet",
+    surname: "Bakay",
+    age: 38,
+    eye: "brown",
+    hobby: "poetry",
+    picture: "./pictures/ismetbakay.jpg",
+  },
+  {
+    name: "Arwa",
+    surname: "Alhussein",
+    age: 35,
+    eye: "brown",
+    hobby: "coding",
+    picture: "./pictures/orwa.jpg",
+  },
+  {
+    name: "Olena",
+    surname: "Smetiuk",
+    age: 25,
+    eye: "green",
+    hobby: "reading",
+    picture: "./pictures/olena.jpg",
+  },
+  {
+    name: "Murat",
+    surname: "Güldibi",
+    age: 28,
+    eye: "black",
+    hobby: "tracking",
+    picture: "./pictures/murat.jpg",
+  },
+
+  {
+    name: "Serkan",
+    surname: "Taşgin",
+    age: 41,
+    eye: "black",
+    hobby: "travel",
+    picture: "./pictures/serkan.jpg",
+  },
+  {
+    name: "Senay",
+    surname: "Atli",
+    age: 23,
+    eye: "black",
+    hobby: "snowboard",
+    picture: "./pictures/senay.jpg",
+  },
+
+  {
+    name: "Yusuf",
+    surname: "İlhan",
+    age: 37,
+    eye: "brown",
+    hobby: "hiking",
+    picture: "./pictures/Yusuf.jpg",
+  },
+  {
+    name: "Emin",
+    surname: "Yilmaz",
+    age: 33,
+    eye: "brown",
+    hobby: "reading",
+    picture: "./pictures/emin.jpg",
+  },
+  {
+    name: "Yuliia",
+    surname: "Kuts",
+    age: 34,
+    eye: "green",
+    hobby: "cooking",
+    picture: "./pictures/julia.jpg",
+  },
+];
+
+document.getElementById("card-container").innerHTML = studentList //burdan aşağısı dom
+  .map((student) => {
+    return `
+   
+   <div class="card-content" onclick = "benimFonksiyonum('${student.name}')">
+          <figure>
+            <img src=${student.picture} alt="" />
+          </figure>
+          <p>
+           <strong>Name:</strong> ${student.name}
+          </p>
+          <p>
+          <strong>Surname:</strong> ${student.surname}
+            
+          </p>
+          <p><strong>Eye Color:</strong> ${student.eye} </p>
+        </div>
+   `;
+  })
+  .join("");
+
+function benimFonksiyonum(str) {
+  alert(str);
+}
+
