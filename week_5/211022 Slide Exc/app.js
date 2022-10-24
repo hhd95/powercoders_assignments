dff7a27e2c55dc6d5bfca82ef08fa198dcd73d07
hhd95 <hasan.danaci@powercoders.org>
2022-10-24 09:04:30 +0200

    myportfolio added

--- week_5/211022 Slide Exc/app.js
+++ week_5/211022 Slide Exc/app.js
@@ -1,6 +1,14 @@
 let a = document.getElementById("navigasyon");
 console.log(a);
 
+function letterChanges() {
+  for (let i = 0; i < 2; i++) {
+    let text = ` headerContent ${i + 1}`;
+    a.innerText += text;
+  }
+}
+letterChanges();
+
 // a.innerHTML = "newly arrived cars";
 console.log(a);
 
@@ -9,5 +17,13 @@ console.log();
 
 // b.innerHTML = "new models";
 
-let p = document.getElementsByTagName("p");
+let p = document.getElementsByTagName("li");
 
+let text = "";
+function deliler() {
+  for (let i = 0; i < p.length - 1; i++) {
+    text = `pContent ${i + 1}`;
+    p[i].innerText = text;
+  }
+}
+deliler();
