32353ffb50938692b62a8e3d37bd54c90d99c01b
hhd95 <hasan.danaci@powercoders.org>
2022-10-24 15:51:09 +0200

    portfolio - todolist

---
+++ week6/exc/app.js
@@ -0,0 +1,4 @@
+// document.querySelector("h2").removeEventListener("click", writeInConsole);
++document.querySelector("h2").removeEventListener("click", writeInConsole);
+let(h4) +
+  document.querySelector("h2").removeEventListener("click", writeInConsole);
